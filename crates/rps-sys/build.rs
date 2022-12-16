d54e96593dbbd9ee79bfae2a109dd7091ec3c8a2
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2022-12-16 13:59:14 -0800

    Build and link RPS core

--- crates/rps-sys/build.rs
+++ crates/rps-sys/build.rs
@@ -15,4 +15,32 @@ fn main() {
         .expect("Failed to generate bindings")
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Failed to write bindings file");
+
+    let mut build = cc::Build::new();
+
+    build
+        .cpp(true)
+        .flag("-std=c++14")
+        .include("RenderPipelineShaders/include")
+        .include("RenderPipelineShaders/src")
+        // Core
+        .file("RenderPipelineShaders/src/core/rps_core.cpp")
+        .file("RenderPipelineShaders/src/core/rps_device.cpp")
+        .file("RenderPipelineShaders/src/core/rps_graph.cpp")
+        .file("RenderPipelineShaders/src/core/rps_result.cpp")
+        // Frontend
+        .file("RenderPipelineShaders/src/frontend/rps_builder.cpp")
+        // Runtime
+        .file("RenderPipelineShaders/src/runtime/common/rps_access.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_format.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_null_runtime_backend.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_null_runtime_device.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_render_graph.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_render_graph_builder.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_render_graph_diagnostics.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_runtime_backend.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_runtime_device.cpp")
+        .file("RenderPipelineShaders/src/runtime/common/rps_subprogram.cpp");
+
+    build.compile("rps");
 }
